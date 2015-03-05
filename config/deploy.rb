f345141666540c70723465a2daf1df9d9d45ac5a
Aaron F Stanton <afstanton@gmail.com>
2015-03-05 12:23:34 -0500

    AIMS-155 Share sunspot config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,7 @@ set :deploy_to, '/home/app/annex-ims'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/sunspot.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
