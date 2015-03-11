b2bb9234ed04a2a95d432e160cf022631d9c13e9
Aaron F Stanton <afstanton@gmail.com>
2015-03-11 15:02:15 -0400

    AIMS-157 That didn't work.

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-SunspotConfig = Sunspot::Rails::Configuration.new
-Sunspot.session = Sunspot::IndexQueue::SessionProxy.new(
-  Sunspot::IndexQueue.new(
-    :session => Sunspot::Session.new{|config| config.solr.url = "http://#{SunspotConfig.hostname}:#{SunspotConfig.port}#{SunspotConfig.path}"}
-  )
-)
