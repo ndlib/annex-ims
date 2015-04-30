3aaaed96fb99f216aa62bcd308f40ca079fe67c5
Aaron F Stanton <afstanton@gmail.com>
2015-04-30 11:11:45 -0400

    AIMS-11 - Fix it so items are added to existing batch if present.

--- app/services/build_batch.rb
+++ app/services/build_batch.rb
@@ -14,22 +14,23 @@ class BuildBatch
     if user.batches.where(active: true).count == 0
       batch = Batch.new
       batch.user = user
-
-      batch_data.each do |data|
-
-        lexed_data = data.split('-')
-
-        request = Request.find(lexed_data[0])
-        batch.requests << request
-
-        item = Item.find(lexed_data[1])
-        batch.items << item
-      end
-
-      batch.save!
+      batch.active = true
     else
-      batch = nil
+      batch = user.batches.where(active: true).first
     end
+
+    batch_data.each do |data|
+
+      lexed_data = data.split('-')
+
+      request = Request.find(lexed_data[0])
+      batch.requests << request
+
+      item = Item.find(lexed_data[1])
+      batch.items << item
+    end
+
+    batch.save!
 
     return batch
 
