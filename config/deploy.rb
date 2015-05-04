e3462354276ffdae1bacf7b605b5dde67db27f4d
Aaron F Stanton <afstanton@gmail.com>
2015-05-04 08:39:32 -0400

    AIMS-178 Add task to restart sneakers on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,7 @@ namespace :deploy do
     on roles(:web), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
+      execute :rake, 'sneakers:restart'
     end
   end
 
