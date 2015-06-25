92382f8327228374cae468381f47d45f1bf3fa37
Jaron Kennel <jaronkk@gmail.com>
2015-06-25 11:17:34 -0400

    Randomize spec order and fixed one spec that failed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,12 @@ require 'webmock/rspec'
 require 'capybara/rspec'
 
 RSpec.configure do |config|
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
