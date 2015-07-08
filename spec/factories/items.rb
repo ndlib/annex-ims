9f6b9cd9078054e0c20fd0477d0c3941fb513d93
Jaron Kennel <jaronkk@gmail.com>
2015-07-07 20:28:47 -0400

    Item factories weren't random over multiple calls

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,16 +1,16 @@
 FactoryGirl.define do
   factory :item do
     sequence(:barcode) { |n| "000012345#{n}" }
-    title Faker::Lorem.sentence
-    author Faker::Name.name
+    title { Faker::Lorem.sentence }
+    author { Faker::Name.name }
     chron "Vol 1"
     thickness 1
     tray nil
-    bib_number "0037612#{Faker::Number.number(2)}"
-    isbn_issn [true, false].sample ? Faker::Code.isbn : "#{Faker::Number.number(4)}-#{Faker::Number.number(4)}"
-    conditions [Item::CONDITIONS.sample, Item::CONDITIONS.sample, Item::CONDITIONS.sample, Item::CONDITIONS.sample].uniq
-    call_number "#{('A'..'Z').to_a.sample}#{('A'..'Z').to_a.sample}#{Faker::Number.number(4)}.#{('A'..'Z').to_a.sample}#{Faker::Number.number(2)} #{(1900..2014).to_a.sample}"
-    initial_ingest Faker::Date.between(2.days.ago, Date.today)
+    bib_number { "0037612#{Faker::Number.number(2)}" }
+    isbn_issn { [true, false].sample ? Faker::Code.isbn : "#{Faker::Number.number(4)}-#{Faker::Number.number(4)}" }
+    conditions { [Item::CONDITIONS.sample, Item::CONDITIONS.sample, Item::CONDITIONS.sample, Item::CONDITIONS.sample].uniq }
+    call_number { "#{('A'..'Z').to_a.sample}#{('A'..'Z').to_a.sample}#{Faker::Number.number(4)}.#{('A'..'Z').to_a.sample}#{Faker::Number.number(2)} #{(1900..2014).to_a.sample}" }
+    initial_ingest { Faker::Date.between(2.days.ago, Date.today) }
     last_ingest Date::today.to_s
     metadata_status "complete"
   end
