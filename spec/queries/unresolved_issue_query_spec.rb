88e2c8f717d05d319e42c037bfba410a5956e9cf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-09 12:11:12 -0400

    Appeasing the hound

--- spec/queries/unresolved_issue_query_spec.rb
+++ spec/queries/unresolved_issue_query_spec.rb
@@ -1,15 +1,15 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe UnresolvedIssueQuery do
   subject { described_class }
 
-  it 'will query for all issues that have not been resolved' do
+  it "will query for all issues that have not been resolved" do
     relation = double
     expect(relation).to receive(:where).with(resolved_at: nil)
     subject.call({}, relation: relation)
   end
 
-  it 'will allow filtering by barcode' do
+  it "will allow filtering by barcode" do
     issue1 = FactoryGirl.create(:issue)
     issue2 = FactoryGirl.create(:issue)
 
@@ -17,8 +17,8 @@ RSpec.describe UnresolvedIssueQuery do
     FactoryGirl.create(:issue, resolved_at: Time.zone.now)
 
     # Overloading expectations so we don't need
-    expect(subject.call({ barcode: issue1.barcode })).to eq([issue1])
-    expect(subject.call({ barcode: '' })).to eq([issue1, issue2])
+    expect(subject.call(barcode: issue1.barcode)).to eq([issue1])
+    expect(subject.call(barcode: "")).to eq([issue1, issue2])
     expect(subject.call({})).to eq([issue1, issue2])
   end
 end
