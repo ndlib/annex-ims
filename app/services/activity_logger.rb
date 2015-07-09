b5d3c6d2f965ee0169344b82d3042391ee00447f
Robert Fox <rfox2@nd.edu>
2015-07-09 08:34:19 -0400

    AIMS-260 Add stock item logging

--- app/services/activity_logger.rb
+++ app/services/activity_logger.rb
@@ -10,6 +10,10 @@ class ActivityLogger
     call(action: "ApiGetItemMetadata", item: item, params: params, api_response: api_response)
   end
 
+  def self.api_stock_item(item:, params:, api_response:)
+    call(action: "ApiStockItem", item: item, params: params, api_response: api_response)
+  end
+
   def self.api_remove_request(request:, params:, api_response:)
     call(action: "ApiRemoveRequest", request: request, params: params, api_response: api_response)
   end
