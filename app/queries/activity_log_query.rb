c9d6158a67045f367ccbfc70cd922933cefd2e0b
Robert Fox <rfox2@nd.edu>
2015-07-10 11:11:06 -0400

    AIMS-107, AIMS-108, AIMS-109, AIMS-110 Simplify log query methods

--- app/queries/activity_log_query.rb
+++ app/queries/activity_log_query.rb
@@ -2,15 +2,13 @@ module ActivityLogQuery
   module_function
 
   def item_history(item)
-    relation.
-      where("data -> 'item' ->> 'barcode' = ?", item.barcode).
+    for_item(item).
       where(action: ["StockedItem", "UnstockedItem", "CreatedItem", "DestroyedItem"]).
       order(action_timestamp: :desc)
   end
 
   def item_usage(item)
-    relation.
-      where("data -> 'item' ->> 'barcode' = ?", item.barcode).
+    for_item(item).
       where(action: ["ScannedItem", "ShippedItem"]).
       order(action_timestamp: :desc)
   end
@@ -18,5 +16,11 @@ module ActivityLogQuery
   def relation
     ActivityLog.all
   end
+
+  def for_item(item)
+    relation.
+      where("data -> 'item' ->> 'barcode' = ?", item.barcode)
+  end
   private_class_method :relation
+  private_class_method :for_item
 end
