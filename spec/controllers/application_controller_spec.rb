44b97bd76b65261db5a9b9c0635f4a854d6a87f7
Jaron Kennel <jaronkk@gmail.com>
2015-07-10 16:21:58 -0400

    Use the index route to avoid having to draw a custom route.  Added a spec for not being signed in

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,30 +2,41 @@ require "rails_helper"
 
 RSpec.describe ApplicationController, type: :controller do
   controller do
-    def dummy
-      render nothing: true
-    end
-  end
-  let(:user) { FactoryGirl.create(:user) }
-
-  before(:each) do
-    sign_in(user)
-    routes.draw { get "dummy" => "anonymous#dummy" }
-  end
-
-  context "user is not an admin" do
-    let(:user) { FactoryGirl.create(:user, admin: false) }
-
-    it "throws a routing error" do
-      expect { get "dummy" }.to raise_error(ActionController::RoutingError)
+    def index
+      render text: "index"
     end
   end
 
-  context "user is an admin" do
-    let(:user) { FactoryGirl.create(:user, admin: true) }
+  context "user" do
+    let(:user) { FactoryGirl.create(:user) }
 
-    it "does not throw a routing error" do
-      expect { get "dummy" }.not_to raise_error
+    before(:each) do
+      sign_in(user)
+    end
+
+    context "is not an admin" do
+      let(:user) { FactoryGirl.create(:user, admin: false) }
+
+      it "throws a routing error" do
+        expect { get :index }.to raise_error(ActionController::RoutingError)
+      end
+    end
+
+    context "is an admin" do
+      let(:user) { FactoryGirl.create(:user, admin: true) }
+
+      it "renders" do
+        get :index
+        expect(response).to be_success
+        expect(response.body).to eq("index")
+      end
+    end
+  end
+
+  context "no user" do
+    it "redirects the user to sign in" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
