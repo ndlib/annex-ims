81452195d70899e013c32e0cb25265d9e11afa6f
Justin Gondron <jgondron@nd.edu>
2015-07-10 11:05:14 -0400

    AIMS-275: -Changed ApplicationController to raise a 404 if the user is not an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :redirect_to_sign_in, unless: :user_signed_in?
+  before_action :redirect_to_unauthorized, unless: :user_admin?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -9,5 +10,17 @@ class ApplicationController < ActionController::Base
 
   def redirect_to_sign_in
     redirect_to new_user_session_path
+  end
+
+  def redirect_to_unauthorized
+    raise_404("User not an admin")
+  end
+
+  def raise_404(message = "Not Found")
+    fail ActionController::RoutingError.new(message)
+  end
+
+  def user_admin?
+    current_user.admin
   end
 end
