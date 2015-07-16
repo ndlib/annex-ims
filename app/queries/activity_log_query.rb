f0dbf79ab824048e89bef8b1ede8aafb77d4b80a
Robert Fox <rfox2@nd.edu>
2015-07-16 15:40:22 -0400

    AIMS-284 Add shelf queries to activity log query class

--- app/queries/activity_log_query.rb
+++ app/queries/activity_log_query.rb
@@ -1,6 +1,12 @@
 module ActivityLogQuery
   module_function
 
+  def shelf_history(shelf)
+    for_shelf(shelf).
+      where(action: ["AssociatedTrayAndShelf", "DissociatedTrayAndShelf", "ShelvedTray", "UnshelvedTray"]).
+      order(action_timestamp: :desc)
+  end
+
   def item_history(item)
     for_item(item).
       where(action: ["StockedItem", "UnstockedItem", "CreatedItem", "DestroyedItem"]).
@@ -21,6 +27,13 @@ module ActivityLogQuery
     relation.
       where("data -> 'item' ->> 'barcode' = ?", item.barcode)
   end
+
+  def for_shelf(shelf)
+    relation.
+      where("data -> 'shelf' ->> 'barcode' = ?", shelf.barcode)
+  end
+
   private_class_method :relation
   private_class_method :for_item
+  private_class_method :for_shelf
 end
