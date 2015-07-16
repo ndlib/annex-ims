f9b82c63465279cb0df499e2569bce0d5050c3ea
Robert Fox <rfox2@nd.edu>
2015-07-16 15:37:52 -0400

    AIMS-284 Add reports controller

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,14 @@
+class ReportsController < ApplicationController
+
+  def call_report
+    barcode = params[:report][:barcode]
+
+    if IsItemBarcode.call(barcode)
+      redirect_to item_detail_path(barcode)
+    elsif IsTrayBarcode.call(barcode)
+      redirect_to tray_detail_path(barcode)
+    elsif IsShelfBarcode.call(barcode)
+      redirect_to shelf_detail_path(barcode)
+    end
+  end
+end
