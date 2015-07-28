e5be54de8e215891fcfdd6b9221ca47e1ae71c63
Justin Gondron <jgondron@nd.edu>
2015-07-28 09:16:26 -0400

    AIMS-295: -Added a new aleph_error to issues and modified the post stock to log this issue if it receives a 422.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ActiveRecord::Base
-  ISSUE_TYPES = ["not_found", "not_for_annex"]
+  ISSUE_TYPES = ["not_found", "not_for_annex", "aleph_error"]
 
   validates :barcode, presence: true
   validates :issue_type, presence: true, inclusion: ISSUE_TYPES
--- app/services/api_post_stock_item.rb
+++ app/services/api_post_stock_item.rb
@@ -16,11 +16,18 @@ class ApiPostStockItem
     if response.success?
       response
     else
-      raise ApiStockItemError, "Error sending stock request to API. params: #{params}, response: #{response.attributes}"
+      handle_error(response)
     end
   end
 
   private
+
+  def handle_error(response)
+    if response.status_code == 422
+      AddIssue.call(item: item, user: nil, type: "aleph_error")
+    end
+    raise ApiStockItemError, "Error sending stock request to API. params: #{params}, response: #{response.attributes}"
+  end
 
   def params
     {
