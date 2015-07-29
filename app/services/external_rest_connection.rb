b6572c2b27b25f5b3f46e0db05d0c15f466af3a6
Robert Fox <rfox2@nd.edu>
2015-07-29 11:11:45 -0400

    AIMS-298 Minor cleanup

--- app/services/external_rest_connection.rb
+++ app/services/external_rest_connection.rb
@@ -97,13 +97,7 @@ class ExternalRestConnection
 
   def process_response
     result = { status: response.status }
-    if response.status == 200
-      result[:results] = JSON.parse(response.body)
-    elsif response.status == 422
-      result[:results] = JSON.parse(response.body)
-    else
-      result[:results] = {}
-    end
+    response.status == (200 || 422) ? result[:results] = JSON.parse(response.body) : result[:results] = {}
     result
   end
 end
