7c2f3161fd87f46d746f03d9f3ca486c2de6b5ee
Jaron Kennel <jaronkk@gmail.com>
2015-09-03 12:24:51 -0400

    Test the "any" criteria type

--- spec/services/search_items_spec.rb
+++ spec/services/search_items_spec.rb
@@ -24,15 +24,59 @@ RSpec.describe SearchItems do
 
   context "barcode" do
     let(:filter) { { criteria_type: "barcode", criteria: item.barcode } }
+
     it "can find an item by barcode" do
       expect(subject).to be_kind_of(Sunspot::Search::StandardSearch)
       expect(results.first).to eq item
     end
   end
 
-  it "returns an empty result" do
-    expect(subject).to be_kind_of(described_class::EmptyResults)
-    expect(results).to eq []
+  context "no filter" do
+    let(:filter) { {} }
+
+    it "returns an empty result" do
+      expect(subject).to be_kind_of(described_class::EmptyResults)
+      expect(results).to eq []
+    end
+  end
+
+  describe "criteria_type" do
+    describe "any" do
+      let(:filter) { { criteria_type: "any" } }
+
+      [
+        :barcode,
+        :bib_number,
+        :call_number,
+        :isbn_issn,
+        :title,
+        :author,
+      ].each do |field|
+        it "searches #{field}" do
+          allow(item).to receive(field).and_return("#{field} value")
+          Sunspot.index(item)
+          Sunspot.commit
+          filter[:criteria] = item.send(field)
+          expect(results.first).to eq(item)
+        end
+      end
+
+      it "searches the tray barcode" do
+        item.tray = FactoryGirl.create(:tray)
+        Sunspot.index(item)
+        Sunspot.commit
+        filter[:criteria] = item.tray.barcode
+        expect(results.first).to eq(item)
+      end
+
+      it "searches the shelf barcode" do
+        item.shelf = FactoryGirl.create(:shelf)
+        Sunspot.index(item)
+        Sunspot.commit
+        filter[:criteria] = item.shelf.barcode
+        expect(results.first).to eq(item)
+      end
+    end
   end
 
 end
