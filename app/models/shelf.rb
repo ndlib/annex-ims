4f6d74cc3600263ebb4e237c8dbb9d6f52720718
Robert Fox <rfox2@nd.edu>
2015-10-28 15:52:45 -0400

    AIMS-334 Add transfer relation for shelf model

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -3,6 +3,7 @@ class Shelf < ActiveRecord::Base
   validates :barcode, uniqueness: true
   validate :has_correct_prefix
 
+  has_one :transfer
   has_many :trays
   has_many :items, through: :trays
   has_many :location_activity_logs, class_name: "ActivityLog", foreign_key: "location_shelf_id"
