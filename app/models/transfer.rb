c1c6c73f94e91029e1f7aed9943c88d3fb5c3c3b
Robert Fox <rfox2@nd.edu>
2015-10-29 14:33:41 -0400

    AIMS-334 Hound

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -2,7 +2,5 @@ class Transfer < ActiveRecord::Base
   belongs_to :shelf
   belongs_to :initiated_by, class_name: "User"
 
-  validates_presence_of :shelf
-  validates_presence_of :initiated_by
-  validates_presence_of :transfer_type
+  validates :shelf, :initiated_by, :transfer_type, presence: true
 end
