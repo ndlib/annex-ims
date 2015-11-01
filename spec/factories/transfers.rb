b6093570e7b5d176f05b2f5d379f80b0c0f1b101
Robert Fox <rfox2@nd.edu>
2015-10-31 22:56:27 -0400

    AIMS-334 Factory tweak

--- spec/factories/transfers.rb
+++ spec/factories/transfers.rb
@@ -2,7 +2,10 @@ FactoryGirl.define do
   factory :transfer do
     transfer_type "ShelfTransfer"
     # sequence(:shelf_id) { |n| "#{n}" }
-    initiated_by User.new(username: "test")
+    sequence(:initiated_by) do |n|
+      User.new(username: "test#{n}").save!
+      User.last
+    end
     sequence(:shelf) do |n|
       Shelf.new(barcode: "SHELF-AL99999999#{n}").save!
       Shelf.last
