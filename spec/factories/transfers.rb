b654c222d61bde0b36a1dbb8cd05e7ba5a6a1496
Robert Fox <rfox2@nd.edu>
2015-10-31 22:26:24 -0400

    AIMS-334 Correct transfer factory

--- spec/factories/transfers.rb
+++ spec/factories/transfers.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :transfer do
     transfer_type "ShelfTransfer"
-    sequence(:shelf_id) { |n| "#{n}" }
+    # sequence(:shelf_id) { |n| "#{n}" }
     initiated_by User.new(username: "test")
+    sequence(:shelf) { |n|
+       Shelf.new(barcode: "SHELF-AL99999999#{n}").save!
+       Shelf.last
+     }
   end
 end
