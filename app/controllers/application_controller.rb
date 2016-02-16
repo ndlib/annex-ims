4342a3c2c20814b13565bf7eca91b5cb73acadd1
BiigY <yturekes@nd.edu>
2016-02-16 12:14:44 -0500

    Bugs were fixed for AIMS-333
    Updates for Justin's feedback on some bugs in the project.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :user_admin?, :raise_404
+  helper_method :user_admin?
   protected
 
   def check_authentication
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_admin 
+  before_action :require_admin
   def index
     @users = User.all
   end
