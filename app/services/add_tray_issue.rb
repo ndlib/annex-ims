9345aa63c218117d3c26fca42e869009eaa975de
Robert Fox <rfox2@nd.edu>
2016-06-17 16:47:57 -0400

    AIMS-356 Add tray issue service class

---
+++ app/services/add_tray_issue.rb
@@ -0,0 +1,37 @@
+class AddTrayIssue
+  attr_reader :user, :tray, :issue_type, :message
+
+  def self.call(user:, tray:, type:, message: nil)
+    new(user: user, tray: tray, type: type, message: message).add
+  end
+
+  def initialize(user:, tray:, type:, message:)
+    @user = user
+    @tray = tray
+    @issue_type = type
+    @message = message
+  end
+
+  def add
+    if valid?
+      issue = TrayIssue.find_or_initialize_by(barcode: barcode, issue_type: issue_type, message: message, resolved_at: nil)
+      new_record = issue.new_record?
+      issue.user = user
+      issue.save!
+      # if new_record
+      #   ActivityLogger.create_issue(tray: tray, issue: issue, user: user)
+      # end
+      issue
+    end
+  end
+
+  private
+
+  def barcode
+    tray.barcode
+  end
+
+  def valid?
+    IsTrayBarcode.call(barcode)
+  end
+end
---
+++ spec/services/add_tray_issue_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe AddTrayIssue do
+  let(:tray) { FactoryGirl.create(:tray) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:issue_type) { "incorrect_count" }
+  let(:message) { "tray was not found" }
+  subject { described_class.call(tray: tray, user: user, type: issue_type, message: message) }
+
+  it "creates an issue with the correct type, barcode, message and user" do
+    expect { subject }.to change { TrayIssue.count }.from(0).to(1)
+    expect(subject.user).to eq(user)
+    expect(subject.issue_type).to eq(issue_type)
+    expect(subject.barcode).to eq(tray.barcode)
+    expect(subject.message).to eq(message)
+  end
+  #
+  # it "logs the tray issue creation" do
+  #   expect(ActivityLogger).to receive(:create_tray_issue).with(tray: tray, issue: kind_of(TrayIssue), user: user)
+  #   subject
+  # end
+
+  it "does not create a second issue for the same type" do
+    issue = described_class.call(tray: tray, user: user, type: issue_type, message: message)
+    expect(subject).to eq(issue)
+  end
+
+  it "creates a second issue for a different type" do
+    issue = described_class.call(tray: tray, user: user, type: "not_valid_barcode", message: message)
+    expect(subject).to_not eq(issue)
+  end
+
+  it "creates a second for a different tray" do
+    other_tray = FactoryGirl.create(:tray)
+    issue = described_class.call(tray: other_tray, user: user, type: issue_type, message: message)
+    expect(subject).to_not eq(issue)
+  end
+end
