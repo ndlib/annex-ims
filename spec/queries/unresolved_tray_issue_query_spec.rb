b23605e986ed61df875834964bf42672d6245b98
Robert Fox <rfox2@nd.edu>
2016-06-21 15:05:18 -0400

    AIMS-356 Add issue and detail reports for trays

---
+++ spec/queries/unresolved_tray_issue_query_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+RSpec.describe UnresolvedTrayIssueQuery do
+  subject { described_class }
+
+  it "will query for all tray_issues that have not been resolved" do
+    relation = double
+    expect(relation).to receive(:where).with(resolved_at: nil)
+    subject.call({}, relation: relation)
+  end
+
+  it "will allow filtering by barcode" do
+    tray_issue1 = FactoryGirl.create(:tray_issue)
+    tray_issue2 = FactoryGirl.create(:tray_issue)
+
+    # This one won't show up in the response object
+    FactoryGirl.create(:tray_issue, resolved_at: Time.zone.now)
+
+    # Overloading expectations so we don't need
+    expect(subject.call(barcode: tray_issue1.barcode)).to eq([tray_issue1])
+    expect(subject.call(barcode: "")).to eq([tray_issue1, tray_issue2])
+    expect(subject.call({})).to eq([tray_issue1, tray_issue2])
+  end
+end
