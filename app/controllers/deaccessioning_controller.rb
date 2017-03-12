50093c5e84d3865e64401eb69012bebbe8b79078
Aaron F Stanton <afstanton@gmail.com>
2017-03-12 13:50:56 -0400

    AIMS-388 refactor similar code into shared partial.

--- app/controllers/deaccessioning_controller.rb
+++ app/controllers/deaccessioning_controller.rb
@@ -2,6 +2,6 @@ class DeaccessioningController < ApplicationController
   before_action :require_admin
 
   def index
-
+    @params = params  # Because we need to fill in the form with previous values.
   end
 end
