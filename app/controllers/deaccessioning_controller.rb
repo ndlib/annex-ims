4a2418883126c85ce7fb13154e9e877e1c63eff7
Aaron F Stanton <afstanton@gmail.com>
2017-03-28 14:20:45 -0400

    Initial work on search results

--- app/controllers/deaccessioning_controller.rb
+++ app/controllers/deaccessioning_controller.rb
@@ -2,6 +2,9 @@ class DeaccessioningController < ApplicationController
   before_action :require_admin
 
   def index
+    @res = SearchItems.call(params)
+    @results = @res.results
+    @total = @res.total
     @params = params  # Because we need to fill in the form with previous values.
   end
 end
