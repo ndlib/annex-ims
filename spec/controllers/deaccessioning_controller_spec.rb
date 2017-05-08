90f760978bb8bda19ee8c2c498105c4642e1d518
Aaron F Stanton <afstanton@gmail.com>
2017-05-08 01:05:10 -0400

    add req tests

--- spec/controllers/deaccessioning_controller_spec.rb
+++ spec/controllers/deaccessioning_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DeaccessioningController, type: :controller do
   let(:user) { FactoryGirl.create(:user, admin: true) }
+  let(:item) { FactoryGirl.create(:item) }
 
   before(:each) do
     sign_in(user)
@@ -13,6 +14,19 @@ RSpec.describe DeaccessioningController, type: :controller do
         get :index
         expect(response).to have_http_status(:success)
       end
+    end
+  end
+
+  describe "POST req" do
+    subject { post :req, items: {"#{item.id}" => "items[#{item.id}]"} }
+    it "builds a deaccessioning request" do
+      expect(BuildDeaccessioningRequest).to receive(:call).with(item.id.to_s)
+      subject
+    end
+
+    it "redirects to batches path" do
+      subject
+      expect(response).to redirect_to(batches_path)
     end
   end
 end
