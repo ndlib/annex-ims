e189bd7ac67a23fa097d3bf56f54b827693b8191
Aaron F Stanton <afstanton@gmail.com>
2017-05-22 05:52:06 -0400

    Fix logic, because unstocked is not the same as not stocked. There's also shipped.

--- app/controllers/deaccessioning_controller.rb
+++ app/controllers/deaccessioning_controller.rb
@@ -11,16 +11,16 @@ class DeaccessioningController < ApplicationController
   def req
     params[:items].keys.each do |item_id|
       item = Item.find(item_id)
-      if item.stocked?
-        BuildDeaccessioningRequest.call(item_id,
-                                        params[:disposition_id],
-                                        params[:comment])
-      else
+      if item.unstocked?
         bin = GetBinFromBarcode.call("BIN-DEAC-HAND-01")
 	SetItemDisposition.call(item_id, params[:disposition_id])
 	item.bin = bin
 	item.save!
 	ActivityLogger.associate_item_and_bin(item: item, bin: bin, user: current_user)
+      else
+        BuildDeaccessioningRequest.call(item_id,
+                                        params[:disposition_id],
+                                        params[:comment])
       end
     end
     redirect_to batches_path
