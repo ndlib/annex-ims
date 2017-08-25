419fc8f1cee0f2f3e07e424f9f7e63225ed9df82
Aaron F Stanton <afstanton@gmail.com>
2017-08-24 20:40:47 -0400

    Add default select, require valid disposition.

--- app/controllers/deaccessioning_controller.rb
+++ app/controllers/deaccessioning_controller.rb
@@ -9,20 +9,25 @@ class DeaccessioningController < ApplicationController
   end
 
   def req
-    params[:items].keys.each do |item_id|
-      item = Item.find(item_id)
-      if item.unstocked?
-        bin = GetBinFromBarcode.call("BIN-DEAC-HAND-01")
-	SetItemDisposition.call(item_id, params[:disposition_id])
-	item.bin = bin
-	item.save!
-	ActivityLogger.associate_item_and_bin(item: item, bin: bin, user: current_user)
-      else
-        BuildDeaccessioningRequest.call(item_id,
-                                        params[:disposition_id],
-                                        params[:comment])
+    if Disposition.pluck(:id).include?(params[:disposition_id])
+      params[:items].keys.each do |item_id|
+        item = Item.find(item_id)
+        if item.unstocked?
+          bin = GetBinFromBarcode.call("BIN-DEAC-HAND-01")
+          SetItemDisposition.call(item_id, params[:disposition_id])
+          item.bin = bin
+          item.save!
+          ActivityLogger.associate_item_and_bin(item: item, bin: bin, user: current_user)
+        else
+          BuildDeaccessioningRequest.call(item_id,
+                                          params[:disposition_id],
+                                          params[:comment])
+        end
       end
+      redirect_to batches_path
+    else
+      flash[:error] = "Select a Disposition"
+      redirect_to deaccessioning_path
     end
-    redirect_to batches_path
   end
 end
