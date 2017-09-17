480c26bfaf85c4874abcee66cd3f7e85ffc03f68
Aaron F Stanton <afstanton@gmail.com>
2017-09-17 14:58:17 -0400

    AIMS-419 Handle no items

--- app/controllers/deaccessioning_controller.rb
+++ app/controllers/deaccessioning_controller.rb
@@ -10,16 +10,21 @@ class DeaccessioningController < ApplicationController
 
   def req
     if Disposition.pluck(:id).include?(params[:disposition_id].to_i)
-      params[:items].keys.each do |item_id|
-        item = Item.find(item_id)
-        request = BuildDeaccessioningRequest.call(item_id,
-                                                  params[:disposition_id],
-                                                  params[:comment])[0]
-        if !item.stocked?
-          DeaccessionNotStockedItem.call(request.id, item_id, params[:disposition_id], current_user)
+      if params[:items].blank?
+        flash[:error] = "No items were selected to create a deaccessioning request."
+        redirect_to deaccessioning_path(params) and return
+      else
+        params[:items].keys.each do |item_id|
+          item = Item.find(item_id)
+          request = BuildDeaccessioningRequest.call(item_id,
+                                                    params[:disposition_id],
+                                                    params[:comment])[0]
+          if !item.stocked?
+            DeaccessionNotStockedItem.call(request.id, item_id, params[:disposition_id], current_user)
+          end
         end
+        redirect_to batches_path and return
       end
-      redirect_to batches_path and return
     else
       flash[:error] = "Select a Disposition"
       redirect_to deaccessioning_path and return
