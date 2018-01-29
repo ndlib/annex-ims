e45e3d7efdf819dcc3ab929cc7364b9bbb62b14f
Aaron F Stanton <afstanton@gmail.com>
2018-01-28 22:32:22 -0500

    AIMS-432 wip that I don't want to lose (again). Need to add tests.

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -3,5 +3,30 @@ class UpdateController < ApplicationController
 
   def index
     @old_item = Item.new
+    @new_item = Item.new
+  end
+
+  def old
+    if IsItemBarcode.call(params[:old_barcode])
+      @old_item = Item.where(barcode: params[:old_barcode]).take
+    else
+      flash[:error] = 'barcode is not an item'
+      redirect_tp update_path
+      return
+    end
+
+    if @old_item.blank?
+      flash[:error] = "No item was found with barcode #{params[old_:barcode]}"
+      redirect_to update_path
+      return
+    end
+
+    redirect_to show_old_update_path(id: @old_item.id)
+    return
+  end
+
+  def show_old
+    @old_item = Item.find(params[:id])
+    @new_item = Item.new
   end
 end
