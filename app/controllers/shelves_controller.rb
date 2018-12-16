c2d02611e217dbcdcdc35e8ebab42c727d2ad392
Aaron F Stanton <afstanton@gmail.com>
2018-12-16 18:56:10 -0500

    AIMS-459 turn scanned tray green

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -119,4 +119,40 @@ class ShelvesController < ApplicationController
       return
     end
   end
+
+  def validate_trays
+    @shelf = Shelf.find(params[:id])
+    tray_barcode = params[:barcode]
+    tray = Tray.where(barcode: tray_barcode).take
+    @scanned = params[:scanned].present? ? params[:scanned] : []
+
+    if tray.nil?
+      if IsValidTray.call(tray_barcode)
+        flash[:error] = I18n.t("errors.barcode_not_found", barcode: tray_barcode)
+        tray = Tray.create!(barcode: tray_barcode)
+        ActivityLogger.create_tray(tray: tray, user: current_user)
+        AddIssue.call(tray: tray,
+                      user: current_user,
+                      type: "shelf_mismatch",
+                      message: "Tray failed QC. Was physically in shelf '#{@shelf.barcode}', but the tray did not exist.")
+      else
+        flash[:error] = I18n.t("errors.barcode_not_valid", barcode: tray_barcode)
+      end
+      render :check_trays
+      return
+    end
+
+    if @shelf.trays.include?(tray)
+      @scanned.push(tray_barcode)
+    else
+      but_message = tray.shelf.present? ? "but is associated with shelf '#{tray.shelf.barcode}'" : "but is not associated with a shelf."
+      AddIssue.call(tray: tray,
+                    user: current_user,
+                    type: "shelf_mismatch",
+                    message: "Tray failed QC. Was physically in shelf '#{@shelf.barcode}', #{but_message}")
+      flash[:error] = I18n.t("errors.barcode_not_associated_to_shelf", barcode: tray_barcode)
+    end
+
+    render :check_trays
+  end
 end
