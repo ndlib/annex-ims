47bbb078a75a88552955b6ce26d8d8909ae975dd
Aaron F Stanton <afstanton@gmail.com>
2019-05-31 19:12:58 -0400

    AIMS-481 Freeze string, maybe it will help stabilize tests?

--- app/services/is_tray_barcode.rb
+++ app/services/is_tray_barcode.rb
@@ -1,7 +1,7 @@
 module IsTrayBarcode
   codes = TrayType.pluck(:code)
   codes_str = codes.join(")|(")
-  PREFIX = "(TRAY-)((#{codes_str}))"
+  PREFIX = "(TRAY-)((#{codes_str}))".freeze
   def self.call(barcode)
     (barcode =~ /^#{PREFIX}(.*)/ ) ? true : false
   end
