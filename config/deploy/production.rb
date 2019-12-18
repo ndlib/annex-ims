0e04d1ef2eaef3000b8a256b90cca02060eca435
ialford <ialford@nd.edu>
2019-12-18 10:25:11 -0500

    update host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server "annex-prod.lc.nd.edu", user: "app", roles: %w{web app db}
+server "annex-prod-okta.lc.nd.edu", user: "app", roles: %w{web app db}
 
 set :rack_env, :production
 
