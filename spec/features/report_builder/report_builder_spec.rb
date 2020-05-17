c208865f2dc7298f0212205ec86869e3adf1de4d
Aaron F Stanton <afstanton@gmail.com>
2020-05-17 16:22:52 -0400

    rubocop

--- spec/features/report_builder/report_builder_spec.rb
+++ spec/features/report_builder/report_builder_spec.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-feature "Report Builder", type: :feature do
+feature 'Report Builder', type: :feature do
   include AuthenticationHelper
 
-  describe "as a worker" do
+  describe 'as a worker' do
     before(:each) do
       login_worker
       visit root_path
     end
 
-    it "can see Report Builder in the menu" do
-      click_link "Reports"
-      expect(page).to have_content "Report Builder"
+    it 'can see Report Builder in the menu' do
+      click_link 'Reports'
+      expect(page).to have_content 'Report Builder'
     end
 
-    it "can access the Report Builder page" do
-      click_link "Reports"
-      click_link "Report Builder"
+    it 'can access the Report Builder page' do
+      click_link 'Reports'
+      click_link 'Report Builder'
       expect(current_path).to eq(reports_path)
-      expect(page).to have_content "Report Builder"
+      expect(page).to have_content 'Report Builder'
     end
   end
 end
