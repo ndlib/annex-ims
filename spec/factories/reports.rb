1f2560ec89762de147289ebed0665a4158f48ae5
Aaron F Stanton <afstanton@gmail.com>
2020-05-17 16:25:12 -0400

    rubocop

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :report do
-    name "MyString"
-    fields "MyText"
-    start_date "2020-05-10"
-    end_date "2020-05-10"
-    activity "MyString"
-    status "MyString"
+    name 'MyString'
+    fields 'MyText'
+    start_date '2020-05-10'
+    end_date '2020-05-10'
+    activity 'MyString'
+    status 'MyString'
   end
 end
